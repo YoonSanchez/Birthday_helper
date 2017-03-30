a51e4bcd0c9982bab0bcc56449b477c566343b3f
Unknown <jonsanchez19@gmail.com>
2017-03-30 19:02:12 +0200

    Generado el proyecto.

---
+++ Birthday_helper/app/src/main/java/com/egibide/a8fdi11/birthday/MainActivity.java
@@ -0,0 +1,13 @@
+package com.egibide.a8fdi11.birthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
